992375c2a7ed8a2b78dec344e3e0e846388ca6ad
ArtyomXXX <ambartsumyanartyomxxx@gmail.com>
2024-07-16 10:15:05 +0300

    Поменял ссылку github pages

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,11 @@
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
+    document.body.style.overflow = "hidden";
 }
   
   function closeNav() {
     document.getElementById("myNav").style.width = "0%";
+    document.body.style.overflow = "auto";
 }
 
 function checkScreenWidth() {
